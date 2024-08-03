a5269bf6b6a3058e9c394b4f66633d3a132e5043
jaimeabbariao <jabbariao@figma.com>
2024-08-02 20:36:59 -0400

    updating with copilot

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,3 +1,4 @@
 return {
   { "nvimdev/dashboard-nvim", enabled = false },
+  { "folke/noice.nvim", enabled = false },
 }
--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -3,6 +3,9 @@ return {
     "ibhagwan/fzf-lua",
     config = function()
       require("fzf-lua").setup({
+        files = {
+          git_icons = false,
+        },
         grep = {
           rg_glob = true,
           glob_flag = "--iglob",
