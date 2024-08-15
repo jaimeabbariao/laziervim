6d2275cf02f39f132dd342649d855387c52536d1
jaimeabbariao <jabbariao@figma.com>
2024-08-14 20:04:15 -0400

    updating some stuff with neovim config

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -3,6 +3,9 @@ return {
     "ibhagwan/fzf-lua",
     config = function()
       require("fzf-lua").setup({
+        winopts = {
+          fullscreen = true,
+        },
         files = {
           git_icons = false,
         },
