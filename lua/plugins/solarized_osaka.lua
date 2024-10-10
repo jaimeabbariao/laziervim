ec099aecca54bd47216cf2543dc5affa43c738f4
jaimeabbariao <jabbariao@figma.com>
2024-10-10 09:42:25 -0400

    updating colorscheme

--- lua/plugins/solarized_osaka.lua
+++ lua/plugins/solarized_osaka.lua
@@ -1,11 +1,10 @@
 return {
   {
     "craftzdog/solarized-osaka.nvim",
-    enabled = false,
     opts = {
       -- your configuration comes here
       -- or leave it empty to use the default settings
-      transparent = true, -- Enable this to disable setting the background color
+      transparent = false, -- Enable this to disable setting the background color
       terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
       styles = {
         -- Style to be applied to different syntax groups
@@ -15,8 +14,8 @@ return {
         functions = {},
         variables = {},
         -- Background styles. Can be "dark", "transparent" or "normal"
-        sidebars = "transparent", -- style for sidebars, see below
-        floats = "transparent", -- style for floating windows
+        sidebars = "dark", -- style for sidebars, see below
+        floats = "dark", -- style for floating windows
       },
       sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
       day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
---
+++ lua/plugins/supermaven.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "supermaven-inc/supermaven-nvim",
+    enabled = false,
+    config = function()
+      require("supermaven-nvim").setup({})
+    end,
+  },
+}
