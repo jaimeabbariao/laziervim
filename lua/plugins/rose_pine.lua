a33266ece0c1b3e70599e0b916c9399b7d5ccdd6
jaimeabbariao <jabbariao@figma.com>
2024-10-30 10:41:22 -0400

    updating some config/colorscheme stuff

--- lua/plugins/rose_pine.lua
+++ lua/plugins/rose_pine.lua
@@ -3,7 +3,7 @@ return {
     "rose-pine/neovim",
     name = "rose-pine",
     opts = {
-      variant = "auto", -- auto, main, moon, or dawn
+      variant = "dawn", -- auto, main, moon, or dawn
       dark_variant = "main", -- main, moon, or dawn
       dim_inactive_windows = false,
       extend_background_behind_borders = true,
