b42761afcfee833382e680b5de15ad4434278e1b
Jaime Abbariao <163903584+jaimeabbariao@users.noreply.github.com>
2025-01-20 22:56:15 -0500

    updating stuff

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,24 @@
 return {
   {
     "aktersnurra/no-clown-fiesta.nvim",
-    -- priority = 1000,
-    -- lazy = false,
     enabled = false,
+    -- lazy = false,
+    -- priority = 1000,
     config = function()
-      require("no-clown-fiesta").setup()
+      require("no-clown-fiesta").setup({
+        transparent = false, -- Enable this to disable the bg color
+        styles = {
+          -- You can set any of the style values specified for `:h nvim_set_hl`
+          comments = {},
+          functions = {},
+          keywords = {},
+          lsp = {},
+          match_paren = {},
+          type = {},
+          variables = {},
+        },
+      })
+
       -- vim.cmd([[colorscheme no-clown-fiesta]])
     end,
   },
@@ -14,9 +27,12 @@ return {
     -- lazy = false,
     -- priority = 1000,
     enabled = false,
-    -- config = function()
-    --   vim.cmd([[colorscheme falcon]])
-    -- end,
+    config = function()
+      -- vim.g.falcon_background = 1
+      -- vim.g.falcon_inactive = 1
+      --
+      -- vim.cmd([[colorscheme falcon]])
+    end,
   },
   {
     "wurli/cobalt.nvim",
@@ -40,6 +56,8 @@ return {
   {
     "mellow-theme/mellow.nvim",
     enabled = false,
+    -- lazy = false,
+    -- priority = 1000,
     config = function()
       -- vim.cmd([[colorscheme mellow]])
     end,
@@ -47,6 +65,7 @@ return {
   {
     "rose-pine/neovim",
     name = "rose-pine",
+    enabled = false,
     config = function()
       require("rose-pine").setup({
         variant = "auto", -- auto, main, moon, or dawn
@@ -112,15 +131,118 @@ return {
 
         before_highlight = function(group, highlight, palette)
           -- Disable all undercurls
-          if highlight.undercurl then
-            highlight.undercurl = false
-          end
+          -- if highlight.undercurl then
+          --     highlight.undercurl = false
+          -- end
+          --
+          -- Change palette colour
+          -- if highlight.fg == palette.pine then
+          --     highlight.fg = palette.foam
+          -- end
         end,
       })
 
-      vim.cmd("colorscheme rose-pine-main")
+      -- vim.cmd("colorscheme rose-pine-main")
       -- vim.cmd("colorscheme rose-pine-moon")
       -- vim.cmd("colorscheme rose-pine-dawn")
+    end,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    enabled = false,
+    config = function()
+      require("kanagawa").setup({
+        compile = false, -- enable compiling the colorscheme
+        undercurl = true, -- enable undercurls
+        commentStyle = { italic = false },
+        functionStyle = {},
+        keywordStyle = { italic = false },
+        statementStyle = { bold = true },
+        typeStyle = {},
+        transparent = false, -- do not set background color
+        dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+        terminalColors = true, -- define vim.g.terminal_color_{0,17}
+        colors = { -- add/modify theme and palette colors
+          palette = {},
+          theme = {
+            wave = {},
+            lotus = {},
+            dragon = {},
+            all = {
+              ui = {
+                bg_gutter = "none",
+              },
+            },
+          },
+        },
+        overrides = function(colors)
+          local theme = colors.theme
+          return {
+            NormalFloat = { bg = "none" },
+            FloatBorder = { bg = "none" },
+            FloatTitle = { bg = "none" },
+
+            -- Save an hlgroup with dark background and dimmed foreground
+            -- so that you can use it where your still want darker windows.
+            -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+            NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+
+            -- Popular plugins that open floats will link to NormalFloat by default;
+            -- set their background accordingly if you wish to keep them dark and borderless
+            LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+            MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+          }
+        end,
+        theme = "wave", -- Load "wave" theme when 'background' option is not set
+        background = { -- map the value of 'background' option to a theme
+          dark = "wave", -- try "dragon" !
+          light = "lotus",
+        },
+      })
+
+      -- setup must be called before loading
+      -- vim.cmd("colorscheme kanagawa")
+    end,
+  },
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      transparent = false, -- Enable this to disable setting the background color
+      terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+      styles = {
+        -- Style to be applied to different syntax groups
+        -- Value is any valid attr-list value for `:help nvim_set_hl`
+        comments = { italic = true },
+        keywords = { italic = true },
+        functions = {},
+        variables = {},
+        -- Background styles. Can be "dark", "transparent" or "normal"
+        sidebars = "dark", -- style for sidebars, see below
+        floats = "dark", -- style for floating windows
+      },
+      sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+      day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+      hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+      dim_inactive = false, -- dims inactive windows
+      lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+
+      --- You can override specific color groups to use other groups or a hex color
+      --- function will be called with a ColorScheme table
+      ---@param colors ColorScheme
+      on_colors = function(colors) end,
+
+      --- You can override specific highlights to use other groups or a hex color
+      --- function will be called with a Highlights and ColorScheme table
+      ---@param highlights Highlights
+      ---@param colors ColorScheme
+      on_highlights = function(highlights, colors) end,
+    },
+    config = function()
+      vim.cmd([[colorscheme solarized-osaka]])
     end,
   },
 }
